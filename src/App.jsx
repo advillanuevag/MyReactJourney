c835c7e5c4bc1893f71ed6fbdb58ab8025f90c05
advillanuevag <vil24014@byui.edu>
2025-01-26 11:26:29 -0300

    Addin dynamic values to DOM by using curly braces to insert values within HTML code and render it dynamicaly.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
+const reactDescriptions = ['Fundamental', 'Crucial', 'Core']; //This code creates an array of values
+
+function genRandomInt(max) {
+  return Math.floor(Math.random() * (max + 1)); //This function generates a random index for
+                                                //later traverse the array created previously
+}
+
 function Header() {
   return (
     <header>
       <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
       <h1>React Essentials</h1>
       <p>
-        Fundamental React concepts you will need for almost any app you are going to build!
+        {reactDescriptions[genRandomInt(2)]} React concepts you will need for almost any app you are going to build!
       </p>
     </header>
   );
