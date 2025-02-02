3f672c2a59465be51a1dc572f360355364aaa0c9
advillanuevag <vil24014@byui.edu>
2025-02-02 12:47:15 -0300

    Creating a Component called CoreConcept and adding Props as a parameter within the Component using Dot notation.

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,14 @@
-import reactImg from "../src/assets/react-core-concepts.png"; //This code is a leaner 
-                                                          // way to insert images in your app.
-                                                          // and then in line 21, you use
-                                                          // {} syntax to insert the image
-                                                          // associated with reactImg variable.
+import reactImg from "../src/assets/react-core-concepts.png"; 
+import componenstImg from "./assets/components.png";                                                           
 
-const reactDescriptions = ['Fundamental', 'Crucial', 'Core']; //This code creates an array of values
+const reactDescriptions = ['Fundamental', 'Crucial', 'Core']; 
 
 function genRandomInt(max) {
-  return Math.floor(Math.random() * (max + 1)); //This function generates a random index for
-                                                //later traverse the array created previously
+  return Math.floor(Math.random() * (max + 1)); 
 }
 
 function Header() {
-  const description = reactDescriptions[genRandomInt(2)] //This line keeps the jsx code cleaner,
-                                                        // and easy to understand by using the
-                                                        // variable description to hold the call
-                                                        // of the genRandomInt() function.
+  const description = reactDescriptions[genRandomInt(2)] 
   return (
     <header>
       <img src={reactImg} alt="Stylized atom" />
@@ -27,13 +20,42 @@ function Header() {
   );
 }
 
+function CoreConcept(props){ 
+  return(
+    <li>
+      <img src={props.image} alt={props.title}/> 
+      <h3>{props.title}</h3>
+      <p>{props.description}</p>
+
+
+    </li>
+  )
+}
+
 
 function App() {
   return (
     <div>
       <Header />
       <main>
-        <h2>Time to get started!</h2>
+        <section id = "core-concepts">
+        <h2>Core Concepts</h2>
+        <ul>
+          <CoreConcept //Here you define the Props you wanna use, within the call to your component CoreConcepts */
+              title = "Components" 
+              description = "The Core IU Building Block"
+              image = {componenstImg} //Here you use {} syntax to insert images as a Prop.
+          />
+          <CoreConcept title = "Props"/>
+          <CoreConcept />
+          <CoreConcept />
+
+
+        </ul>
+
+
+        </section>
+        
       </main>
     </div>
   );
