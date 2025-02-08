3389eb9b609aca36469a64db3df60295b257d18c
advillanuevag <vil24014@byui.edu>
2025-02-08 11:36:30 -0300

    Using JS Destructuring feature to set CORE_CONCEPTS values as Props for CoreConcept Component.

--- src/App.jsx
+++ src/App.jsx
@@ -24,12 +24,13 @@ function Header() {
   );
 }
 
-function CoreConcept(props){ 
+function CoreConcept({ image, title, description }){ /*Here we use Destructuring JS feature
+  to set the CORE_CONCEPTS values as Props for the CoreConcept Component. */
   return(
     <li>
-      <img src={props.image} alt={props.title}/> 
-      <h3>{props.title}</h3>
-      <p>{props.description}</p>
+      <img src={image} alt={title}/> 
+      <h3>{title}</h3>
+      <p>{description}</p>
 
 
     </li>
